fa0b7e9cb5cd48ffd2291dd301118cd3e17ff53a
swjoon <jetkid1446@gmail.com>
2024-11-18 16:36:26 +0900

    명언제조기

---
+++ src/main/java/practice/LifeQuotes.java
@@ -0,0 +1,12 @@
+package practice;
+
+import java.util.Scanner;
+
+public class LifeQuotes {
+    public static void main(String[] args)throws Exception{
+        Scanner sc = new Scanner(System.in);
+
+
+
+    }
+}
